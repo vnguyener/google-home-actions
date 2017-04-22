39630d79b23e4a20a7f5725c85aa22effcfbc93a
Vu Nguyen <its.vnguyen@gmail.com>
2017-04-22 14:40:08 -0500

    update package.json, added yams, updated services for mongoose

--- grocery-list/services/grocery.service.js
+++ grocery-list/services/grocery.service.js
@@ -2,9 +2,10 @@
 
 const mongoose = require("mongoose"),
     q = require("q"),
+    Grocery = require('../models/grocery.model'),
     config = require("../configs/database.json");
 
-const uri = config.env.dev.clearGroceryList;
+const uri = config.env.dev.groceryList;
 
 // database connection
 mongoose.connect(uri, (err, res) => {
@@ -25,23 +26,104 @@ process.on("SIGINT", () => {
 // exports
 module.exports = {
     getGroceryList: getGroceryList,
-    removeGroceryItem: removeGroceryItem,
-    addGroceryItem: addGroceryItem,
-    clearGroceryList: clearGroceryList
+    removeGroceryItems: removeGroceryItems,
+    addGroceryItems: addGroceryItems,
+    clearGroceryList: clearGroceryList,
+    checkGroceryList: checkGroceryList
 };
 
 function getGroceryList() {
+    let deferred = q.defer();
 
+    Grocery.find({}, (err, items) => {
+        if (error) {
+            console.log(error);
+            deferred.reject(error);
+        }
+
+        if (items) {
+            deferred.resolve(items);
+        }
+    });
+
+    return deferred.promise;
 };
 
-function removeGroceryItem() {
+function checkGroceryList(item) {
+    let deferred = q.defer();
 
+    Grocery.findOne({ "name": item }, (error, res) => {
+        if (error) {
+            console.log(error);
+            deferred.reject(error);
+        }
+
+        if (res) {
+            deferred.resolve(res);
+        };
+
+    });
+
+    return deferred.promise;
 };
 
-function addGroceryItem() {
+function removeGroceryItems(items) {
+    let deferred = q.defer();
 
+    items.forEach((item) => {
+        Grocery.findOneAndRemove({ "name": item }, (err, res) => {
+            if (err) {
+                console.log(err);
+                deferred.reject(err);
+            } 
+
+            if (res) {
+                deferred.resolve(res);
+            }
+        });
+    });
+
+    return deferred.promise;
+};
+
+function addGroceryItems(items) {
+    let deferred = q.defer();
+    console.log(items);
+
+    items.forEach((item) => {
+        let newGrocery = new Grocery({
+            name: item
+        });
+
+        newGrocery.save((err, res) => {
+            if (err) {
+                console.log(err);
+                deferred.reject(err);
+            }  
+            
+            if (res) {
+                deferred.resolve(res);
+            };
+        });
+
+    });
+
+    return deferred.promise;
 };
 
 function clearGroceryList() {
+    let deferred = q.defer();
 
+    Grocery.remove({}, (err, res) => {
+        if (err) {
+            console.log(err);
+            deferred.reject(err);
+        } 
+
+        if (res) {
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
 };
